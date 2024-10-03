9d79a1bf0816a5c7827f6229e1034205066e2753
Maksim Izmailov <xizmailov@stuba.sk>
2024-10-03 11:13:26 +0200

    cv3

---
+++ README.md
@@ -0,0 +1,2 @@
+create compile_commands.json: colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
--- block1_izmailov/src/teleop_node.cpp
+++ block1_izmailov/src/teleop_node.cpp
@@ -1,8 +1,10 @@
 #include "cstdio"
-#include "iostream"
+#include "exception"
 #include "rclcpp/rclcpp.hpp"
-#include "rrm_msgs/msg/command.hpp"
 #include "rrm_msgs/srv/command.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include <stdexcept>
+#include <system_error>
 
 using namespace std;
 
@@ -10,39 +12,83 @@ class Teleop : public rclcpp::Node {
 public:
   Teleop() : Node("Teleop") {
     RCLCPP_INFO(this->get_logger(), "Teleop initialized");
-    publisher_ =
-        this->create_publisher<rrm_msgs::msg::Command>("move_command", 10);
+    client_ = this->create_client<rrm_msgs::srv::Command>("move_command");
+
+    positions_ = {0.0, 0.0, 0.0};
   }
-  void move(int joint_id, double position) {
-    rrm_msgs::msg::Command message;
-    message.joint_id = joint_id;
-    message.position = position;
-    publisher_->publish(message);
+
+  bool move(const std::vector<double> &positions, double max_velocity) {
+    auto request = std::make_shared<rrm_msgs::srv::Command::Request>();
+    request->positions = positions;
+    std::vector<double> current_positions = positions_;
+    request->velocities =
+        calculate_joint_speeds(current_positions, positions, max_velocity);
+
+    positions_ = positions;
+    auto future = client_->async_send_request(request);
+
+    auto result = rclcpp::spin_until_future_complete(
+        this->get_node_base_interface(), future, std::chrono::seconds(5));
+
+    if (result == rclcpp::FutureReturnCode::SUCCESS) {
+      RCLCPP_INFO(this->get_logger(), "Service call succeeded: %s",
+                  future.get()->message.c_str());
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Service call failed");
+    }
+
+    return true;
+  }
+
+  void connect() {
+    if (!client_->wait_for_service(std::chrono::seconds(10))) {
+      throw std::runtime_error("Service not available...");
+    }
   }
 
 private:
-  rclcpp::Publisher<rrm_msgs::msg::Command>::SharedPtr publisher_;
+  rclcpp::Client<rrm_msgs::srv::Command>::SharedPtr client_;
+  std::vector<double> positions_;
+
+  std::vector<double>
+  calculate_joint_speeds(const std::vector<double> &current_positions,
+                         const std::vector<double> &target_positions,
+                         double max_speed) {
+    std::vector<double> speeds(current_positions.size(), 0.0);
+
+    double min_distance = std::numeric_limits<double>::max();
+    for (size_t i = 0; i < current_positions.size(); ++i) {
+      double distance = std::abs(target_positions[i] - current_positions[i]);
+      min_distance = std::min(min_distance, distance);
+    }
+
+    double time = min_distance / max_speed;
+
+    for (size_t i = 0; i < current_positions.size(); ++i) {
+      double distance = target_positions[i] - current_positions[i];
+      speeds[i] = distance / time;
+    }
+
+    for (double item : speeds) {
+      printf("%f\n", item);
+    }
+
+    return speeds;
+  }
 };
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   Teleop robot;
 
-  while (true) {
-    int joint_id;
-    double position;
-    cout << "zadajte klb:\n";
-    cin >> joint_id;
-
-    if (joint_id == -1) {
-      break;
-    }
-
-    cout << "zadajte polohu:\n";
-    cin >> position;
-
-    robot.move(joint_id, position);
+  try {
+    robot.connect();
+  } catch (const std::system_error &e) {
+    std::cerr << e.what();
+    return 1;
   }
+
+  robot.move({0.4, -0.2, 1}, 0.1);
 
   return 0;
 }
